5fea51455caaf5156529098dac7d9609d353c67e
Cristian andres <andresdevops@gmail.com>
2021-03-03 10:19:38 -0500

    Se presenta en este snapshot la clase (Main) en un estado animico dado a que no conlleva actualmente la lógica, solo se presenta como una clase de objeto para tranferir datos.

---
+++ main.rb
@@ -0,0 +1,17 @@
+
+class Main # calme case
+    #definiendo métodos de lectura & escrituras
+    attr_accesor :objeto; attr_accesor :clase; attr_accesor :ls_kernel # snake case
+
+    def objeto_main
+
+    end 
+
+    def clase_object
+        
+    end    
+
+    def metodo_kernel
+        
+    end 
+end    
